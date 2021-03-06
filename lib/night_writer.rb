55cfd91d8af1fa0c8f3db026ef43f2a5197366ab
Andrew Smith <andrewpatrick138@gmail.com>
2021-03-06 10:32:57 -0800

    add braille text file and experiment with file i/o walkthrough

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,2 +1,11 @@
 class NightWriter
+    test = File.open(ARGV[0], "r")
+    incoming_text = handle.read
+    capitalized_text = incoming_text.upcase
+
+    writer = File.open(ARGV[1], "w")
+
+    writer.write(capitalized_text)
+    
+    writer.close
 end
