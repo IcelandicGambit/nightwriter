dcb2bac46f29c49c07aae24605044bfa52472205
Andrew Smith <andrewpatrick138@gmail.com>
2021-03-08 21:57:11 -0800

    add night_writer and test

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,31 +1,57 @@
-require './lib/alphabet'
-require './lib/translator'
+require_relative 'alphabet'
 
 class NightWriter
-  attr_reader :original_message,
+  attr_reader :input_message,
               :translated_message,
               :alphabet
+              :output_message
 
   def initialize
-    @original_message =  File.open(ARGV[0]).read
+    @input_message = File.open(ARGV[0])
     @translated_message = []
+    @output_message = []
     @alphabet = Alphabet.new
+    @line_one = []
+    @line_two = []
+    @line_three = []
   end
 
-
   def read_message
-     puts @original_message
-    @original_message.split("").each do |letter|
-     @translated_message << @alphabet.english_braille[letter]
+    read_from_file
+    concatenate_lines
+    write_to_file
+  end
+
+  def read_from_file
+    @input_message.each do |line|
+      line.split("").each do |char|
+        unless char == "\n"
+          braille = @alphabet.english_braille[char]
+          @line_one << braille[0]
+          @line_two << braille[1]
+          @line_three << braille[2]
+        end
+      end
     end
-     writer = File.open(ARGV[1], "w")
-     writer.write @translated_message
-     puts "Created #{ARGV[1]} containing #{@original_message.length} characters"
+  end
+
+  def concatenate_lines
+    @line_one = @line_one.join
+    @line_two = @line_two.join
+    @line_three = @line_three.join
+  end
+
+  def write_to_file
+    writer = File.open(ARGV[1], "w")
+    chars_written = 0
+    until @line_one.length == 0
+      temp_chars_written = @line_one.slice(0..79).length
+      writer.write @line_one.slice!(0..79) + "\n"
+      writer.write @line_two.slice!(0..79) + "\n"
+      writer.write @line_three.slice!(0..79) + "\n"
+      chars_written += temp_chars_written
+    end
+    puts "Created #{ARGV[1]} containing #{chars_written / 2} characters"
   end
 end
 NightWriter.new.read_message
-
-
-
-
-
