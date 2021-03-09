dacf27c86d716ed56d65c956eda5d7787246b0d4
Andrew Smith <andrewpatrick138@gmail.com>
2021-03-09 00:05:47 -0800

    test malfunctioning troubleshoot

--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -1,10 +1,14 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/night_reader'
+require './lib/night_reader'
 
 class NightReaderTest < MiniTest::Test
 
   def setup
+    @input_message = File.open(ARGV[0], "r")
+    @alphabet = Alphabet.new
+    @writer = File.open(ARGV[1], "w+")
     @night_reader = NightReader.new
   end
 
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,10 +1,14 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/night_writer'
+require './lib/alphabet'
 
 class NightWriterTest < MiniTest::Test
 
   def setup
+    @input_message = File.open(ARGV[0], "r")
+    @alphabet = Alphabet.new
+    @writer = File.open(ARGV[1], "w+")
     @night_writer = NightWriter.new
   end
 
